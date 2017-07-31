dd987e012c7bb3f87fa852375666c6b83857e2ee
Waagna <dominik.waagna@gmail.com>
2017-07-31 22:55:03 +0200

    Code refactor, Zt 5-7 in view added

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -35,22 +35,31 @@ export class AppComponent {
   }
 
   onZtChanged(event: Event, i: number) {
-    if (event.srcElement.id.startsWith('zt1')) {
-      this.wursts[i].zt2 = (this.wursts[i].zt1 / this.wursts[i].antzt1) * this.wursts[i].antzt2;
-      this.wursts[i].zt3 = (this.wursts[i].zt1 / this.wursts[i].antzt1) * this.wursts[i].antzt3;
-      this.wursts[i].zt4 = (this.wursts[i].zt1 / this.wursts[i].antzt1) * this.wursts[i].antzt4;
-    }else if ((event.srcElement.id.startsWith('zt2'))) {
-      this.wursts[i].zt1 = (this.wursts[i].zt2 / this.wursts[i].antzt2) * this.wursts[i].antzt1;
-      this.wursts[i].zt3 = (this.wursts[i].zt2 / this.wursts[i].antzt2) * this.wursts[i].antzt3;
-      this.wursts[i].zt4 = (this.wursts[i].zt2 / this.wursts[i].antzt2) * this.wursts[i].antzt4;
-    }else if ((event.srcElement.id.startsWith('zt3'))) {
-      this.wursts[i].zt1 = (this.wursts[i].zt3 / this.wursts[i].antzt3) * this.wursts[i].antzt1;
-      this.wursts[i].zt2 = (this.wursts[i].zt3 / this.wursts[i].antzt3) * this.wursts[i].antzt2;
-      this.wursts[i].zt4 = (this.wursts[i].zt3 / this.wursts[i].antzt3) * this.wursts[i].antzt4;
-    }else if ((event.srcElement.id.startsWith('zt4'))) {
-      this.wursts[i].zt1 = (this.wursts[i].zt4) * this.wursts[i].antzt1;
-      this.wursts[i].zt2 = (this.wursts[i].zt4) * this.wursts[i].antzt2;
-      this.wursts[i].zt3 = (this.wursts[i].zt4) * this.wursts[i].antzt3;
+    switch (event.srcElement.id) {
+      case 'zt1':
+        const foo1 = this.wursts[i].zt1 / this.wursts[i].antzt1;
+        this.wursts[i].zt2 = foo1 * this.wursts[i].antzt2;
+        this.wursts[i].zt3 = foo1 * this.wursts[i].antzt3;
+        this.wursts[i].zt4 = foo1 * this.wursts[i].antzt4;
+        break;
+      case 'zt2':
+        const foo2 = this.wursts[i].zt2 / this.wursts[i].antzt2;
+        this.wursts[i].zt1 = foo2 * this.wursts[i].antzt1;
+        this.wursts[i].zt3 = foo2 * this.wursts[i].antzt3;
+        this.wursts[i].zt4 = foo2 * this.wursts[i].antzt4;
+        break;
+      case 'zt3':
+        const foo3 = this.wursts[i].zt3 / this.wursts[i].antzt3;
+        this.wursts[i].zt1 = foo3 * this.wursts[i].antzt1;
+        this.wursts[i].zt2 = foo3 * this.wursts[i].antzt2;
+        this.wursts[i].zt4 = foo3 * this.wursts[i].antzt4;
+        break;
+      case 'zt4':
+        const foo4 = this.wursts[i].zt4 / this.wursts[i].antzt4;
+        this.wursts[i].zt1 = foo4 * this.wursts[i].antzt1;
+        this.wursts[i].zt2 = foo4 * this.wursts[i].antzt2;
+        this.wursts[i].zt3 = foo4 * this.wursts[i].antzt3;
+        break;
     }
 
     this.wursts[i].gesamt = this.wursts[i].zt1 + this.wursts[i].zt2 + this.wursts[i].zt3 + this.wursts[i].zt4;
@@ -63,7 +72,7 @@ export class AppComponent {
     for (const item of this.wursts){
       if (item.zt1 != null) {
         this.gesamtZt[0] += item.zt1;
-        }
+      }
       if (item.zt2 != null) {
         this.gesamtZt[1] += item.zt2;
         }
