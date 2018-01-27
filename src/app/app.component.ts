253aaee3e49220c60c100d3e8162a3741fc0a720
Dominik Wagner <dominik.waagna@gmail.com>
2018-01-27 18:26:02 +0100

    Wurst hinzugefügt

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,4 @@
-import { Component, Input, OnChanges, SimpleChange  } from '@angular/core';
+import { Component, Input, OnChanges, SimpleChange } from '@angular/core';
 import { Wurst } from './wurst.model';
 
 @Component({
@@ -13,22 +13,25 @@ export class AppComponent {
 
   // R1, R2, Wiener, Speck, Eis, Brat, Käse, Göderl
   wursts: Wurst[] = [
-    new Wurst('Wiener'        , [0, 10, 50, 0, 0, 20, 0, 20]),
-    new Wurst('Käswurst'      , [0,  0, 75, 0, 0, 25, 0,  0]),
-    new Wurst('Polnische'     , [20, 0, 45, 0, 0, 20, 0, 15]),
-    new Wurst('Cabanossi'     , [ 0, 0, 70, 0, 0, 20, 0, 10]),
-    new Wurst('Käskreiner'    , [ 0, 0, 65, 0, 0,  0, 25, 10]),
-    new Wurst('Bratwurstges.' , [ 0, 0, 60, 0, 0, 20,  0, 20]),
-    new Wurst('Saumeisen'     , [ 0, 0, 60, 0, 0, 20, 0, 20]),
-    new Wurst('Grillwurst'    , [ 0, 0, 40, 0, 0, 40, 0, 20]),
-    new Wurst('Leberkäs'      , [18.19, 0,  24.07, 25.93, 31.48, 0, 0, 0]),
-    new Wurst('Rindsbrat'     , [0, 67,  0, 0, 33, 0, 0, 0]),
-    new Wurst('Weises Brat'   , [ 0, 0, 34, 33, 33, 0, 0, 0]),
+    new Wurst('Wiener', [0, 10, 50, 0, 0, 20, 0, 20]),
+    new Wurst('Käswurst', [0, 0, 75, 0, 0, 25, 0, 0]),
+    new Wurst('Polnische', [20, 0, 45, 0, 0, 20, 0, 15]),
+    new Wurst('Cabanossi', [0, 0, 70, 0, 0, 20, 0, 10]),
+    new Wurst('Käskreiner', [0, 0, 65, 0, 0, 0, 25, 10]),
+    new Wurst('Bratwurstges.', [0, 0, 60, 0, 0, 20, 0, 20]),
+    new Wurst('Saumeisen', [0, 0, 60, 0, 0, 20, 0, 20]),
+    new Wurst('Grillwurst', [0, 0, 40, 0, 0, 40, 0, 20]),
+    new Wurst('Leberkäs', [18.19, 0, 24.07, 25.93, 31.48, 0, 0, 0]),
+    new Wurst('Rindsbrat', [0, 67, 0, 0, 33, 0, 0, 0]),
+    new Wurst('Weises Brat', [0, 0, 34, 33, 33, 0, 0, 0]),
     // new Wurst('Braunschweiger', [ 0, 0, 40, 0, 0, 40, 0, 20]),
-    new Wurst('Jagdwurst'     , [20, 0, 40, 0, 0, 20, 0, 20]),
-    new Wurst('Wildwurst'     , [ 0, 0, 60, 0, 0, 20, 0, 20]),
-    new Wurst('Speckwurst'    , [ 0, 0,  0, 0, 0,16.67,0,83.33]),
-    new Wurst('Jalapena'      , [ 0, 0,58.82,0,0,23.53,0,17.65])
+    new Wurst('Jagdwurst', [20, 0, 40, 0, 0, 20, 0, 20]),
+    new Wurst('Wildwurst', [0, 0, 60, 0, 0, 20, 0, 20]),
+    new Wurst('Speckwurst', [0, 0, 0, 0, 0, 16.67, 0, 83.33]),
+    new Wurst('Jalapena', [0, 0, 58.82, 0, 0, 23.53, 0, 17.65]),
+    new Wurst('Tiroler ger.', [0, 0, 10, 0, 0, 4, 0, 0]),
+    new Wurst('Knoblauchw.',  [0, 0, 10, 0, 0, 3, 0, 3]),
+    new Wurst('Zwiebelw.',    [0, 0, 10, 0, 0, 4, 0, 3])
   ];
 
   gesamtZt: number[] = [
@@ -59,7 +62,7 @@ export class AppComponent {
   }
   calculateGesamtZt() {
     this.gesamtZt.fill(0);
-    for (const item of this.wursts){
+    for (const item of this.wursts) {
       for (let i = 0; i < item.zt.length; i++) {
         if (item.zt[i] != null) {
           this.gesamtZt[i] += item.zt[i];
