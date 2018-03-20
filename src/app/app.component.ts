6507e1370ecf6317bad734ee06b06490c7973445
Dominik Wagner <dominik.waagna@gmail.com>
2018-03-20 22:32:54 +0100

    New Wurst added, Print Button added

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -29,10 +29,11 @@ export class AppComponent {
     new Wurst('Wildwurst',    [0, 0, 0, 60, 0, 0, 20, 0, 20]),
     new Wurst('Speckwurst',   [0, 0, 0, 0, 0, 0, 16.67, 0, 83.33]),
     new Wurst('Jalapena',     [0, 0, 0, 58.82, 0, 0, 23.53, 0, 17.65]),
-    new Wurst('Tiroler ger.', [0, 0, 0, 71.42, 0, 0, 28.58, 0, 0]),
+    new Wurst('Tiroler ger.', [0, 0, 71.42, 0, 0, 0, 28.58, 0, 0]),
     new Wurst('Knoblauchw.',  [0, 0, 0, 62.5, 0, 0, 18.75, 0, 18.75]),
     new Wurst('Zwiebelw.',    [0, 0, 0, 58.82, 0, 0, 23.53, 0, 17.65]),
-    new Wurst('ges. Wildbratwurst', [1, 1, 1, 1, 1, 1, 1, 1, 1])
+    new Wurst('ges. Wildbratwurst', [1, 1, 1, 1, 1, 1, 1, 1, 1]),
+    new Wurst('Krakauer',     [0, 0, 71.42, 0, 0, 0, 28.58, 0, 0])
   ];
 
   gesamtZt: number[] = [
@@ -84,5 +85,9 @@ export class AppComponent {
 
   getNumberfromID(controlIndex: string) {
     return parseInt(controlIndex.substring(2, 3), 10) - 1;
+  }
+
+  printTable() {
+    window.print();
   }
 }
