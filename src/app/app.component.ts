eb7db0a6ae2ca43f353f3a569e9e5e9d66d70b29
wgnrd <dominik.waagna@gmail.com>
2021-06-22 18:50:44 +0200

    impelement recipie changes

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -13,25 +13,24 @@ export class AppComponent {
 
   // R1, R2, S1, Wiener, Speck, Eis, Brat, Käse, Göderl
   sausages: Wurst[] = [
-    new Wurst("Wiener", [0, 10, 0, 50, 0, 0, 20, 0, 20]),
+    new Wurst("Wiener", [0, 0, 0, 60, 0, 0, 20, 0, 20]),
     new Wurst("Käswurst", [0, 0, 0, 75, 0, 0, 0, 25, 0]),
-    new Wurst("Polnische", [20, 0, 45, 0, 0, 0, 20, 0, 15]),
+    new Wurst("Polnische", [0, 0, 65, 0, 0, 0, 20, 0, 15]),
     new Wurst("Cabanossi", [0, 0, 0, 70, 0, 0, 20, 0, 10]),
     new Wurst("Käskreiner", [0, 0, 0, 39, 0, 0, 30, 25, 6]),
-    new Wurst("Bratwurstges.", [0, 0, 0, 70, 0, 0, 0, 0, 30]),
+    new Wurst("Bratwurstges.", [0, 0, 0, 60, 0, 0, 20, 0, 20]),
     new Wurst("Saumeisen", [0, 0, 0, 60, 0, 0, 20, 0, 20]),
     new Wurst("Grillwurst", [0, 0, 0, 40, 0, 0, 40, 0, 20]),
     new Wurst("Leberkäs", [18.19, 0, 0, 24.07, 25.93, 31.48, 0, 0, 0]),
     new Wurst("Rindsbrat", [0, 67, 0, 0, 0, 33, 0, 0, 0]),
     new Wurst("Weises Brat", [0, 0, 0, 34, 33, 33, 0, 0, 0]),
-    new Wurst("Jagdwurst", [20, 0, 0, 40, 0, 0, 20, 0, 20]),
+    new Wurst("Jagdwurst", [0, 0, 0, 60, 0, 0, 20, 0, 20]),
     new Wurst("Wildwurst", [0, 0, 0, 60, 0, 0, 20, 0, 20]),
     new Wurst("Speckwurst", [0, 0, 0, 0, 0, 0, 16.67, 0, 83.33]),
     new Wurst("Jalapena", [0, 0, 0, 58.82, 0, 0, 23.53, 0, 17.65]),
     new Wurst("Tiroler ger.", [0, 0, 71.42, 0, 0, 0, 28.58, 0, 0]),
     new Wurst("Knoblauchw.", [0, 0, 0, 62.5, 0, 0, 18.75, 0, 18.75]),
     new Wurst("Zwiebelw.", [0, 0, 0, 58.82, 0, 0, 23.53, 0, 17.65]),
-    new Wurst("ges. Wildbratwurst", [1, 1, 1, 1, 1, 1, 1, 1, 1]),
     new Wurst("Krakauer", [0, 0, 71.42, 0, 0, 0, 28.58, 0, 0])
   ];
 
@@ -54,16 +53,6 @@ export class AppComponent {
       this.sausages[sausageId].ingredients[ingredientId] / this.sausages[sausageId].antzt[ingredientId];
 
     this.sausages[sausageId].gesamt = 0;
-    if (this.sausages[sausageId].name !== "ges. Wildbratwurst") {
-      for (let j = 0; j < this.sausages[0].ingredients.length; j++) {
-        if (j !== ingredientId) {
-          this.sausages[sausageId].ingredients[j] =
-            this.RoundTwoDigits(basepercent * this.sausages[sausageId].antzt[j]);
-        }
-        this.sausages[sausageId].gesamt += this.RoundTwoDigits(this.sausages[sausageId].ingredients[j]);
-      }
-    }
-
     this.calculateColumnSum();
   }
 
@@ -75,6 +64,7 @@ export class AppComponent {
           this.gesamtZt[i] += item.ingredients[i];
           this.specialKaeswurst(item, i);
         }
+        this.gesamtZt[i] = this.RoundTwoDigits(this.gesamtZt[i]);
       }
     }
   }
